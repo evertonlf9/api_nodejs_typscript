352095989efdf964ac5331638d9e443b67441681
unknown <everton9dev@gmail.com>
2020-10-10 09:51:33 -0300

    Removendo a pasta dist

--- dist/app/routes/address.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const express_1 = require("express");
-const address_1 = tslib_1.__importDefault(require("../controllers/address"));
-const router = express_1.Router();
-router.get('/user/:id/', address_1.default.GetAddress);
-exports.default = router;
